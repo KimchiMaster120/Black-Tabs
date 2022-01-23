9226ae8f0ef0cf9bb1738d52ad5bcba472ae3f6b
KimchiMaster120 <98119357+KimchiMaster120@users.noreply.github.com>
2022-01-23 18:35:27 -0500

    Delete script.js

--- src/script.js
+++
@@ -1,6 +0,0 @@
-function simpleunblocker() {
-    document.getElementById("iframe").src = "http://thesimpleunblocker.com/";
-    document.getElementById("iframe").height = "100%";
-    document.getElementById("iframe").width = "100%";
-    document.getElementById("content").innerHTML = '';
-}
